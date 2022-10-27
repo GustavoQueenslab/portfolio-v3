2922a53bb3337eae84b1d0f35145e98c788b5f68
gustavosf2003 <gsferreira2003@gmail.com>
2022-10-27 01:05:55 +0100

    Add style in the certificates badges

--- components/Certificates.tsx
+++ components/Certificates.tsx
@@ -1,13 +1,79 @@
+import Image from "next/image";
 import Wrapper from "./Wrapper";
-
+import React from "react";
+import Tilt from "./Tilt";
 export default function Certificates() {
   return (
     <Wrapper className="flex flex-col items-center justify-center">
       <h3 className="mb-32 text-2xl text-primary">Certificates</h3>
-      <section className="flex flex-col mt-16 lg:flex-row">
-        <div className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black w-80 text-primary">
-          <div className="absolute self-center w-40 h-40 text-center bg-white -top-32 text-primary">
-            <p>s</p>
+      <section className="flex flex-col gap-40 mt-16 lg:gap-12 lg:flex-row">
+        <div className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black rounded-md w-80 text-primary">
+          <div className="absolute flex items-center self-center text-center -top-32">
+            <Tilt>
+              <Image
+                alt="FIAP"
+                src="/images/certificates/fiap.png"
+                width={120}
+                height={120}
+              />
+            </Tilt>
+          </div>
+          <div>
+            <p>MTA 98-364</p>
+            <p className="mt-4 text-justify break-all">
+              Microsoft certificate of database fundamentals using SQL Server.
+              Click here
+            </p>
+          </div>
+        </div>
+        <div className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black rounded-md w-80 text-primary">
+          <div className="absolute flex items-center self-center text-center -top-32">
+            <Tilt>
+              <Image
+                alt="FIAP"
+                src="/images/certificates/mta-1.png"
+                width={120}
+                height={120}
+              />
+            </Tilt>
+          </div>
+          <div>
+            <p>MTA 98-364</p>
+            <p className="mt-4 text-justify break-all">
+              Microsoft certificate of database fundamentals using SQL Server.
+              Click here
+            </p>
+          </div>
+        </div>
+        <div className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black rounded-md w-80 text-primary">
+          <div className="absolute flex items-center self-center text-center -top-32">
+            <Tilt>
+              <Image
+                alt="FIAP"
+                src="/images/certificates/mta-1.png"
+                width={120}
+                height={120}
+              />
+            </Tilt>
+          </div>
+          <div>
+            <p>MTA 98-364</p>
+            <p className="mt-4 text-justify break-all">
+              Microsoft certificate of database fundamentals using SQL Server.
+              Click here
+            </p>
+          </div>
+        </div>
+        <div className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black rounded-md w-80 text-primary">
+          <div className="absolute flex items-center self-center text-center -top-32">
+            <Tilt>
+              <Image
+                alt="FIAP"
+                src="/images/certificates/helsinki.jpg"
+                width={120}
+                height={120}
+              />
+            </Tilt>
           </div>
           <div>
             <p>MTA 98-364</p>
---
+++ components/Tilt.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+import Title from "react-vanilla-tilt";
+interface TiltProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export default function Tilt({ className, children }: TiltProps) {
+  return (
+    <Title
+      className={clsx(className, "px-8 py-4 bg-white rounded-md")}
+      style={{}}
+    >
+      {children}
+    </Title>
+  );
+}
