ab4741b1681d017aa45b5d22d8867a582b42631c
gustavosf2003 <gsferreira2003@gmail.com>
2022-10-28 00:26:21 +0100

    Add: translate array data with i18n and add carroussel package

--- components/Certificates.tsx
+++ components/Certificates.tsx
@@ -2,87 +2,47 @@ import Image from "next/image";
 import Wrapper from "./Wrapper";
 import React from "react";
 import Tilt from "./Tilt";
+import { useTranslation } from "next-i18next";
+import i18next from "i18next";
+
+interface CertificatesProps {
+  title: string;
+  description: string;
+}
+
 export default function Certificates() {
+  const { t } = useTranslation();
+  const certificates: CertificatesProps[] = t("home:certificatesCards", {
+    returnObjects: true,
+  });
+  console.log(certificates);
   return (
     <Wrapper className="flex flex-col items-center justify-center">
-      <h3 className="mb-32 text-2xl text-primary">Certificates</h3>
+      <h3 className="mb-32 text-2xl text-primary">s</h3>
       <section className="flex flex-col gap-40 mt-16 lg:gap-12 lg:flex-row">
-        <div className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black rounded-md w-80 text-primary">
-          <div className="absolute flex items-center self-center text-center -top-32">
-            <Tilt>
-              <Image
-                alt="FIAP"
-                src="/images/certificates/fiap.png"
-                width={120}
-                height={120}
-              />
-            </Tilt>
+        {certificates.map((certificate, i) => (
+          <div
+            key={i}
+            className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black rounded-md w-80 text-primary"
+          >
+            <div className="absolute flex items-center self-center text-center -top-32">
+              <Tilt>
+                <Image
+                  alt={certificate.title}
+                  src="/images/certificates/fiap.png"
+                  width={120}
+                  height={120}
+                />
+              </Tilt>
+            </div>
+            <div>
+              <p>{certificate.title}</p>
+              <p className="mt-4 text-justify break-all">
+                {certificate.description}
+              </p>
+            </div>
           </div>
-          <div>
-            <p>MTA 98-364</p>
-            <p className="mt-4 text-justify break-all">
-              Microsoft certificate of database fundamentals using SQL Server.
-              Click here
-            </p>
-          </div>
-        </div>
-        <div className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black rounded-md w-80 text-primary">
-          <div className="absolute flex items-center self-center text-center -top-32">
-            <Tilt>
-              <Image
-                alt="FIAP"
-                src="/images/certificates/mta-1.png"
-                width={120}
-                height={120}
-              />
-            </Tilt>
-          </div>
-          <div>
-            <p>MTA 98-364</p>
-            <p className="mt-4 text-justify break-all">
-              Microsoft certificate of database fundamentals using SQL Server.
-              Click here
-            </p>
-          </div>
-        </div>
-        <div className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black rounded-md w-80 text-primary">
-          <div className="absolute flex items-center self-center text-center -top-32">
-            <Tilt>
-              <Image
-                alt="FIAP"
-                src="/images/certificates/mta-1.png"
-                width={120}
-                height={120}
-              />
-            </Tilt>
-          </div>
-          <div>
-            <p>MTA 98-364</p>
-            <p className="mt-4 text-justify break-all">
-              Microsoft certificate of database fundamentals using SQL Server.
-              Click here
-            </p>
-          </div>
-        </div>
-        <div className="relative flex flex-col px-5 pt-10 pb-5 text-center bg-black rounded-md w-80 text-primary">
-          <div className="absolute flex items-center self-center text-center -top-32">
-            <Tilt>
-              <Image
-                alt="FIAP"
-                src="/images/certificates/helsinki.jpg"
-                width={120}
-                height={120}
-              />
-            </Tilt>
-          </div>
-          <div>
-            <p>MTA 98-364</p>
-            <p className="mt-4 text-justify break-all">
-              Microsoft certificate of database fundamentals using SQL Server.
-              Click here
-            </p>
-          </div>
-        </div>
+        ))}
       </section>
     </Wrapper>
   );
