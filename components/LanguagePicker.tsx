9149c0f26bfdf6479079c44f27b4ec2679668625
gustavosf2003 <gsferreira2003@gmail.com>
2022-11-12 16:04:08 +0000

    Add: Language picker

--- components/LanguagePicker.tsx
+++ components/LanguagePicker.tsx
@@ -1,43 +1,63 @@
-import { useState } from "react";
+import clsx from "clsx";
+import { useEffect, useRef, useState } from "react";
 import LanguageOption from "./LanguageOption";
+import LanguagePlaceholder from "./LanguagePlaceholder";
 
-export default function LanguagePicker() {
+interface LanguagePickerProps {
+  className?: string;
+}
+
+export default function LanguagePicker({ className }: LanguagePickerProps) {
   const [handleDropdown, setHandleDropdown] = useState(false);
-  return (
-    <>
-      <div className="w-36">
-        <button
-          className="w-full "
-          onClick={() => setHandleDropdown(!handleDropdown)}
-        >
-          GB EN {"<"}
-        </button>
-        {handleDropdown && (
-          <div className="flex flex-col items-center mt-2">
-            <div className="inline-block w-3 -mt-2 overflow-hidden">
-              <div className="w-2 h-2 origin-bottom-left transform rotate-45 bg-white"></div>
-            </div>
+  const languagePickerRef = useRef(null);
 
-            <div className="flex flex-col items-center gap-1 py-1 text-black bg-white rounded-md cursor-pointer">
-              <LanguageOption
-                flag="/images/flags/sweden.png"
-                language="Swedish"
-                locale="sv"
-              />
-              <LanguageOption
-                flag="/images/flags/portugal.png"
-                language="Portuguese"
-                locale="pt"
-              />
-              <LanguageOption
-                flag="/images/flags/uk.png"
-                language="English"
-                locale="en"
-              />
-            </div>
+  useOnClickOutside(languagePickerRef, () => setHandleDropdown(false));
+  return (
+    <div className={clsx("w-36 ", className)} ref={languagePickerRef}>
+      <LanguagePlaceholder event={() => setHandleDropdown(!handleDropdown)} />
+      {handleDropdown && (
+        <div className="flex flex-col items-center mt-20">
+          <div className="w-3 -mt-2 overflow-hidden ">
+            <div className="w-2 h-2 origin-bottom-left transform rotate-45 bg-white"></div>
           </div>
-        )}
-      </div>
-    </>
+
+          <div className="flex flex-col items-center gap-1 py-1 text-black bg-white rounded-md cursor-pointer">
+            <LanguageOption
+              flag="/images/flags/uk.png"
+              language="English"
+              locale="en"
+            />
+            <LanguageOption
+              flag="/images/flags/portugal.png"
+              language="Portuguese"
+              locale="pt"
+            />
+            <LanguageOption
+              flag="/images/flags/sweden.png"
+              language="Swedish"
+              locale="sv"
+            />
+          </div>
+        </div>
+      )}
+    </div>
   );
+}
+
+// Hook reference:  https://usehooks.com/useOnClickOutside/
+function useOnClickOutside(ref, handler: (event: Event) => void) {
+  useEffect(() => {
+    const listener = (event) => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,9 +5,16 @@ import LanguagePicker from "./LanguagePicker";
 export default function Navbar() {
   const { t } = useTranslation();
   return (
-    <nav className="sticky top-0 z-40 h-20 px-10 py-4 text-white bg-secondary">
-      <p>{t("home:name")}</p>
-      <LanguagePicker />
+    <nav className="sticky top-0 z-40 flex items-center justify-between h-20 px-10 py-4 text-white bg-secondary">
+      <div className="flex items-center pl-3 text-xl uppercase border-l-8 border-blue-400 lg:basis-2/5">
+        <p>Gustavo Ferreira</p>
+      </div>
+      <div className="justify-center hidden text-xl uppercase gap-44 lg:basis-full lg:flex">
+        <p>{t("home:competences")}</p>
+        <p>{t("home:projects")}</p>
+        <p>{t("home:contact")}</p>
+      </div>
+      <LanguagePicker className="lg:basis-1/5" />
     </nav>
   );
 }
