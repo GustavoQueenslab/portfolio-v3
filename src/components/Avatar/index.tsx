d4067f22c2e5a40879887c61f33cc95475f8e72b
Gustavo Ferreira <gustavo@queenslab.pt>
2023-08-20 02:56:19 +0100

    fix: introduction component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,41 @@
+import { useRef, useState } from "react";
+
+import Image from "next/image";
+
+import { useOnClickOutside } from "../hooks/OnClickOutside";
+
+enum MOOD_ITEMS {
+  HAPPY = "happy",
+  ANGRY = "angry",
+  NAUSEOUS = "nauseous",
+}
+
+const AVATAR_IMAGES = {
+  [MOOD_ITEMS.HAPPY]: "/images/avatar/happy.svg",
+  [MOOD_ITEMS.ANGRY]: "/images/avatar/angry.svg",
+  [MOOD_ITEMS.NAUSEOUS]: "/images/avatar/nauseous.svg",
+};
+
+export default function AvatarPicker() {
+  const [avatarMood, setAvatarMood] = useState(MOOD_ITEMS.HAPPY);
+  const avatarRef = useRef(null);
+
+  useOnClickOutside(avatarRef, () => setAvatarMood(MOOD_ITEMS.HAPPY));
+
+  return (
+    <div
+      onMouseOver={() => setAvatarMood(MOOD_ITEMS.ANGRY)}
+      onMouseOut={() => setAvatarMood(MOOD_ITEMS.HAPPY)}
+      onClick={() => setAvatarMood(MOOD_ITEMS.NAUSEOUS)}
+    >
+      <Image
+        alt="Avatar"
+        src={AVATAR_IMAGES[avatarMood]}
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: "100%", height: "auto" }}
+      />
+    </div>
+  );
+}
--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,17 +1,20 @@
 import { useTranslation } from "next-i18next";
 
-import AvatarPicker from "./AvatarPicker";
+import AvatarPicker from "../Avatar";
+
 export default function Introduction() {
   const { t } = useTranslation();
   return (
-    <section className="flex items-center justify-center text-left lg:justify-between lg:px-40 my-14">
-      <AvatarPicker />
-      <article className="flex flex-col gap-4 basis-3/4">
+    <section className="flex items-center justify-center gap-10 text-left lg:justify-between lg:px-40 my-14">
+      <div className="hidden lg:block basis-1/3">
+        <AvatarPicker />
+      </div>
+      <article className="flex flex-col gap-4 basis-2/3">
         <p className="flex items-center text-5xl font-semibold font-quicksand lg:text-6xl">
           {t(`home:aboutTitle`)}
           <span className="text-5xl text-tertiary">.</span>
         </p>
-        <p className="text-2xl text-justify" id="competences">
+        <p className="text-2xl text-center lg:text-justify" id="competences">
           {t(`home:aboutBody`)}
         </p>
       </article>
