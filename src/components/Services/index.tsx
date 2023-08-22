6d2afb9328bb1cef90b4c8ac09b40013358061f9
Gustavo Ferreira <gustavo@queenslab.pt>
2023-08-23 00:45:44 +0100

    feat: services block

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,13 +1,17 @@
 import ServiceCategory from "@/components/Services/ServiceItem";
-import { services } from "@/lib/services";
 
-export default function Services() {
+export default function Services({ services }) {
+  if (!services) return <></>;
+
   return (
     <section className="flex flex-col items-center justify-center py-16">
-      <p className="text-3xl font-bold font-quicksand">Services</p>
+      <p className="text-3xl font-bold font-quicksand">{services.title}</p>
       <div className="flex flex-col flex-wrap gap-8 mt-16 xl:flex-row">
-        {services.map((category) => (
-          <ServiceCategory service={category} key={category.id} />
+        {services.items.map((category, i) => (
+          <ServiceCategory
+            service={{ ...category, item: i }}
+            key={category._uid}
+          />
         ))}
       </div>
     </section>
