ef358592cadaf456678df0f0d416ea00d2220674
GustavoQueenslab <gustavo@queenslab.pt>
2023-12-17 19:12:00 +0000

    feat: google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/index.css";
 import "tailwindcss/tailwind.css";
+import Script from "next/script";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { appWithTranslation } from "next-i18next";
 
@@ -7,9 +9,27 @@ const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_KEY}`}
+        id="google-analytics"
+      />
+
+      <Script strategy="lazyOnload" id="google-analytics-config">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.GOOGLE_ANALYTICS_KEY}', {
+        page_path: window.location.pathname,
+      });
+    `}
+      </Script>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
   );
 }
 export default appWithTranslation(MyApp);
